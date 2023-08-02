f254a5657ff67be25d5489db0e088c50883612e2
5arSanti <santiari05@hotmail.com>
2023-08-02 11:42:28 -0500

    Radio works with the Audio component of expo-av

--- src/components/LinkAudio/index.js
+++ src/components/LinkAudio/index.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Video } from 'expo-av';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { Video, Audio } from 'expo-av';
 
 const LinkAudio = () => {
-    const videoUri = 'https://conexion.fm:8000/stream';
+    const audioUri = 'https://conexion.fm:8000/stream';
+    const sound = new Audio.Sound();
+
+    const playAudio = async () => {
+        try {
+            await sound.loadAsync({uri: audioUri})
+            await sound.playAsync();
+        }
+        catch(err){
+            alert("Sucedio un error: ", err);
+            console.log(err);
+        }
+    }
 
     return (
         <View style={styles.container}>
-            <Video
+
+            <TouchableOpacity onPress={() => playAudio()}>
+                <Text>Reproducir</Text>
+            </TouchableOpacity>
+
+            {/* <Video
                 source={{ uri: videoUri }}
                 shouldPlay
                 useNativeControls
                 style={styles.video}
                 resizeMode="contain"
-            />
+            /> */}
         </View>
     );
 }
